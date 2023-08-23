55ee5728da047c1ec2b8fcf88981ffc75f3841e2
Dumprr <140656627+dumprr@users.noreply.github.com>
2023-08-23 16:29:02 -0400

    Done! 🥳

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# NOT DONE! DO NOT PLAY YET!
-
-
 # JSnake
+
 The iconic "Snake" game made in HTML
 
 Programmed in HTML+CSS and JS!
 
-[CLICK HERE TO PLAY IT WITHOUT SETTING EVERYTHING UP!]()
+[CLICK HERE TO PLAY IT WITHOUT SETTING EVERYTHING UP!](https://dumprr.is-a.dev/JSnake/)
 
+## Gameplay
 
-# Usage
+Use Arrow keys or WASD to move the snake.
+
+There is some sound. (Food and Game Over)
+
+It's very simple and plain.
+
+## Usage
 
 Download the ZIP file of the repository and launch index.html lol that's it
--- snake.js
+++ snake.js
@@ -1,9 +1,13 @@
+// audio
 
+var foodsound = new sound("assets/food.mp3");
+var gameoversound = new sound("assets/gameover.mp3");
 
 // set all variables and stuff
-var blocksize = 25;
+var blocksize = 20;
 var board;
 var context;
+var gameOver = false;
 
 // snake parts
 var SnakeX = blocksize * 5;
@@ -11,6 +15,8 @@ var SnakeY = blocksize * 5;
 
 var Xvel = 0;
 var Yvel = 0;
+
+var SnakeBod = [];
 
 // snek food !
 var FoodX = blocksize * 10;
@@ -22,65 +28,113 @@ window.onload = function() {
     var rows = parseInt(askednumberR);
     console.log(`Rows: ${rows}.`)
   } else {
-    varsQ()
+    location.reload()
   }
   let askednumberC = prompt("Number of columns?", "20");
    if (askednumberC != null) {
     var cols = parseInt(askednumberC);
     console.log(`Columns: ${cols}.`)
   } else {
-    varsQ()
+    location.reload()
+  }
+  let askednumberT = prompt("Milliseconds per update?", "100");
+   if (askednumberT != null) {
+    var time = parseInt(askednumberT);
+    console.log(`Update MS: ${time}.`)
+  } else {
+    location.reload()
   }
   board = document.getElementById("board");
   board.height = blocksize * rows;
   board.width = blocksize * cols;
   context = board.getContext("2d");
 
+  GlobalRows = rows;
+  GlobalCols = cols;
+  GlobalTime = time;
   foodPosition(rows, cols);
   document.addEventListener("keyup", changeDirection);
-  setInterval(update(rows, cols), 150);
+  setInterval(update, time);
 }
 
-function update(rows, cols) {
-    context.fillStyle="black";
+function update() {
+  console.log(`Update in ${GlobalTime}ms`)
+  if (gameOver) {
+    location.reload()
+    return;
+  }
+    context.fillStyle="#190e2c";
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle="red";
     context.fillRect(FoodX, FoodY, blocksize, blocksize)
 
     if (SnakeX == FoodX && SnakeY == FoodY) {
-      foodPosition(rows, cols);
+      foodsound.stop();
+      SnakeBod.push([FoodX, FoodY])
+      foodPosition(GlobalRows, GlobalCols);
+      console.log("Got Food")
+      foodsound.play();
+    }
+
+    for (let i = SnakeBod.length-1; i > 0; i--) {
+      SnakeBod[i] = SnakeBod[i-1];
+    }
+    if (SnakeBod.length) {
+      SnakeBod[0] = [SnakeX, SnakeY];
     }
 
     context.fillStyle="lime";
     SnakeX += Xvel * blocksize;
     SnakeY += Yvel * blocksize;
     context.fillRect(SnakeX, SnakeY, blocksize, blocksize);
+    for (let i = 0; i < SnakeBod.length; i++) {
+      context.fillRect(SnakeBod[i][0], SnakeBod[i][1], blocksize, blocksize);
+    }
+
+    if (SnakeX < 0 || SnakeX > GlobalCols*blocksize || SnakeY < 0 || SnakeY > GlobalRows*blocksize) {
+      gameoversound.play();
+      gameOver = true;
+      console.log("Game Over")
+      alert(`Game Over... Your Score: ${SnakeBod.length}`);
+      alert("Click OK to reload...");
+    }
+
+    for (let i = 0; i < SnakeBod.length; i++) {
+      if (SnakeX == SnakeBod[i][0] && SnakeY == SnakeBod[i][1]) {
+        gameoversound.play();
+        gameOver = true;
+        console.log("Game Over")
+        alert(`Game Over... Your Score: ${SnakeBod.length}`);
+        alert("Click OK to reload...");
+      }
+    }
 }
 
 function changeDirection(e) {
-  if (e.code == "ArrowUp") {
+  console.log(`Keypress | ${e.code}`)
+  if (e.code == "ArrowUp" && Yvel != 1) {
     Xvel = 0;
     Yvel = -1;
-  } else if (e.code == "ArrowDown") {
+  } else if (e.code == "ArrowDown" && Yvel != -1) {
     Xvel = 0;
     Yvel = 1;
-  } else if (e.code == "ArrowLeft") {
+  } else if (e.code == "ArrowLeft" && Xvel != 1) {
     Xvel = -1;
     Yvel = 0;
-  } else if (e.code == "ArrowRight") {
+  } else if (e.code == "ArrowRight" && Xvel != -1) {
     Xvel = 1;
     Yvel = 0;
-  } else if (e.code == "W") {
+  } else if (e.code == "KeyW" && Yvel != 1) {
     Xvel = 0;
     Yvel = -1;
-  } else if (e.code == "S") {
+  } else if (e.code == "KeyS" && Yvel != -1) {
     Xvel = 0;
     Yvel = 1;
-  } else if (e.code == "A") {
+  } else if (e.code == "KeyA" && Xvel != 1) {
     Xvel = -1;
     Yvel = 0;
-  } else if (e.code == "D") {
+  } else if (e.code == "KeyD" && Xvel != -1) {
     Xvel = 1;
     Yvel = 0;
   }
@@ -89,4 +143,18 @@ function changeDirection(e) {
 function foodPosition(rows, cols) {
   FoodX = Math.floor(Math.random() * cols) * blocksize;
   FoodY = Math.floor(Math.random() * rows) * blocksize;
+}
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  this.play = function(){
+      this.sound.play();
+  }
+  this.stop = function(){
+      this.sound.pause();
+  }    
 }
